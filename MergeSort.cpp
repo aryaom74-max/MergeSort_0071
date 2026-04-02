a72d0c00af2249992dec568a637c4f185c206f75
aryaom74-max <aryaom74@gmail.com>
2026-04-02 18:34:21 +0700

    Update MergeSort.cpp

--- MergeSort.cpp
+++ MergeSort.cpp
@@ -47,7 +47,7 @@ void mergeSort(int low, int high)
     // step 3
     // fungsi rekursi - memanggil diri sendiri
     mergeSort(low, mid);      // step 3.a
-    mergeSort(mi + 1, high); // step 3.b
+    mergeSort(mid + 1, high); // step 3.b
 
     // step 4
     int i = low;      // step 4.a
