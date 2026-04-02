dd7bf6471f2591673d056306c8f7c14e5a33226d
aryaom74-max <aryaom74@gmail.com>
2026-04-02 18:41:51 +0700

    Update MergeSort.cpp

--- MergeSort.cpp
+++ MergeSort.cpp
@@ -95,7 +95,7 @@ void output()
     cout << "\nData setelah diurutkan (Merge Sort): ";
     for (int i = 0; i < n; i++)
     {
-        co << arr[i] << " ";
+        cout << arr[i] << " ";
     }
     cout << endl;
 }
