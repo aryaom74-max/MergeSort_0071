44bb06e0825af9506881a870346891d4f1775a8d
aryaom74-max <aryaom74@gmail.com>
2026-04-02 18:33:54 +0700

    Update MergeSort.cpp

--- MergeSort.cpp
+++ MergeSort.cpp
@@ -24,7 +24,7 @@ void input()
     }
 
     cout << "\n-----------------------" << endl;
-    cout << "\nInputkan Isi elemt array" << endl;
+    cout << "\nInputkan Isi element array" << endl;
     cout << "\n-----------------------" << endl;
 
     for (int i = 0; i < n; i++)
